37f836df9f91762b37825a475400e357934e630e
Merlan <merlan01tel@gmail.com>
2024-12-02 00:29:49 +0500

    init

---
+++ Assets/Scripts/IPAddressGame.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class IPAddressGame : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI targetIPText; // Для отображения целевого IP
+    [SerializeField] private TextMeshProUGUI resultText;  // Для подсказки результата
+    private List<string> allIPs = new List<string>(); // Хранит все IP-адреса
+    private string targetIP; // Случайно выбранный целевой IP
+
+    private void Start()
+    {
+        // Находим все объекты с IP адресами
+        IPv4Address[] ipScripts = FindObjectsOfType<IPv4Address>();
+        foreach (IPv4Address script in ipScripts)
+        {
+            // Собираем все IP адреса в список
+
+            allIPs.Add(script.textMeshPro.text);
+        }
+
+        // Проверка наличия IP-адресов
+        if (allIPs.Count == 0)
+        {
+            Debug.LogError("Нет IP-адресов для игры!");
+            return;
+        }
+
+        // Выбираем случайный IP из списка
+        targetIP = allIPs[Random.Range(0, allIPs.Count)];
+
+        // Отображаем целевой IP на экране
+        targetIPText.text = "Найди этот IP: " + targetIP;
+        resultText.text = ""; // Очищаем текст результата
+    }
+
+    public void CheckIPAddress(string playerChoice)
+    {
+        Debug.Log(playerChoice + " " + targetIP);
+        // Сравниваем выбор игрока с целевым IP
+        if (playerChoice == targetIP)
+        {
+            
+            resultText.text = "Правильно! Ты нашел IP: " + targetIP;
+        }
+        else
+        {
+            resultText.text = "Неправильно! Попробуй снова.";
+        }
+    }
+}
--- Assets/Scripts/OnTriggerIPaddress.cs
+++ Assets/Scripts/OnTriggerIPaddress.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class OnTriggerIPaddress : MonoBehaviour
 {
     public GameObject ip;
-    private void OnTriggerEnter(Collider other)
+
+    private IPAddressGame ipaddressGame;
+    private void Start()
+    {
+        ipaddressGame = FindObjectOfType<IPAddressGame>();
+        Debug.Log(ipaddressGame.name);
+    }
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "MainCamera")
         {
             ip.SetActive(true);
         }
+        if (Input.GetKey(KeyCode.E))
+        {
+            ipaddressGame.CheckIPAddress(ip.GetComponentInChildren<TextMeshProUGUI>().text.ToString());
+        }
+        Debug.Log(ip.GetComponentInChildren<TextMeshProUGUI>().text.ToString());
     }
 
     private void OnTriggerExit(Collider other)
